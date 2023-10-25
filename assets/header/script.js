e25329a70922e66c0f5a53905b1a40102efe5b81
Your Name <ryanrob327@gmail.com>
2023-10-25 12:14:36 -0700

    I burnt the cookies ):

--- assets/header/script.js
+++ assets/header/script.js
@@ -1,34 +1,63 @@
-// script.js for the header
-
 document.addEventListener('DOMContentLoaded', function () {
-    // Check if the user is signed in (you can use your authentication logic here)
-    const userEmail = localStorage.getItem('userEmail');
+  // Read the user's email from the cookie
+  const cookies = document.cookie.split('; ');
+  let userEmail = '';
+  for (const cookie of cookies) {
+      const [name, value] = cookie.split('=');
+      if (name === 'userEmail') {
+          userEmail = value;
+          break;
+      }
+  }
 
-    if (userEmail) {
-        // User is signed in, replace "Sign Up" with user's email
-        updateHeader(userEmail);
-    } else {
-        // User is not signed in, keep "Sign Up" link
-        updateHeaderSignUp();
-    }
+  if (userEmail) {
+      // User is signed in, replace "Sign Up" with user's email
+      updateHeader(userEmail);
+  } else {
+      // User is not signed in, keep "Sign Up" link
+      updateHeaderSignUp();
+  }
+
+  displayCookies(cookies);
 });
 
+function displayCookies(cookies) {
+  const cookieDisplay = document.getElementById('cookieDisplay');
+  if (cookieDisplay) {
+      const cookiesList = cookies.map(cookie => {
+          const [name, value] = cookie.split('=');
+          return `${name}: ${value}`;
+      });
+
+      cookieDisplay.innerHTML = cookiesList.join('<br>');
+  }
+}
+
 function updateHeader(email) {
-    const headerRight = document.getElementById('headerRight');
-    headerRight.innerHTML = `
-        <a href="/">Home</a>
-        <a href="/stats/">Statistics</a>
-        <a href="/About">About</a>
-        <span>${email}</span>
-    `;
+  const headerRight = document.getElementById('headerRight');
+  headerRight.innerHTML = `
+      <a href="/">Home</a>
+      <a href="/stats/">Statistics</a>
+      <a href="/About">About</a>
+      <span>${email}</span>
+      <a href="javascript:signOut()">Sign Out</a>
+  `;
 }
 
 function updateHeaderSignUp() {
-    const headerRight = document.getElementById('headerRight');
-    headerRight.innerHTML = `
-        <a href="/">Home</a>
-        <a href="/stats/">Statistics</a>
-        <a href="/About">About</a>
-        <a href="/sign_in">Sign Up</a>
-    `;
+  const headerRight = document.getElementById('headerRight');
+  headerRight.innerHTML = `
+      <a href="/">Home</a>
+      <a href="/stats/">Statistics</a>
+      <a href="/About">About</a>
+      <a href="/sign_in">Sign Up</a>
+  `;
+}
+
+function signOut() {
+  // Remove the "userEmail" cookie
+  document.cookie = 'userEmail=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+
+  // Redirect to the sign-in page or any other desired page
+  window.location.href = '/sign_in';
 }
--- assets/sign-in/script.js
+++ assets/sign-in/script.js
@@ -1,30 +1,31 @@
-// script.js for both sign-in and home pages
-
 function signIn(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    // Get email and password from the form
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+  // Get email and password from the form
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
 
-    // authentication logic (validate email and password)
-    /*
-    if (email == (get email data) && password == (get password data)) {
-        const isAuthenticated = true;
-    }
-    else {
-        const isAuthenticated = false;
-    }
-    */
+  // authentication logic (validate email and password)
+  /*
+  if (email == (get email data) && password == (get password data)) {
+      const isAuthenticated = true;
+  }
+  else {
+      const isAuthenticated = false;
+  }
+  */
 
-    // For demo purposes, let's assume successful authentication
-    const isAuthenticated = true;
+  // For demo purposes, let's assume successful authentication
+  const isAuthenticated = true;
 
-    if (isAuthenticated) {
-        // Redirect to the home page
-        window.location.href = '/';
-    } else {
-        // Display an error message (you can customize this based on your authentication logic)
-        alert('Authentication failed. Please check your email and password.');
-    }
+  if (isAuthenticated) {
+      // Set a cookie with the user's email
+      document.cookie = `userEmail=${email}; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/`;
+
+      // Redirect to the home page
+      window.location.href = '/';
+  } else {
+      // Display an error message (you can customize this based on your authentication logic)
+      alert('Authentication failed. Please check your email and password.');
+  }
 }
