fb50246408acaa7cbaa8d683a781d712dcca4952
EdwinKuttappi <edwinab0007@gmail.com>
2023-10-30 09:55:46 -0700

    transfer script to search

--- assets/header/script.js
+++ assets/header/script.js
@@ -1,147 +1,58 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   // Read the user's email from the cookie
-//   const cookies = document.cookie.split('; ');
-//   let userEmail = '';
-//   for (const cookie of cookies) {
-//       const [name, value] = cookie.split('=');
-//       if (name === 'userEmail') {
-//           userEmail = value;
-//           break;
-//       }
-//   }
+document.addEventListener('DOMContentLoaded', function () {
+  // Read the user's email from the cookie
+  const cookies = document.cookie.split('; ');
+  let userEmail = '';
+  for (const cookie of cookies) {
+      const [name, value] = cookie.split('=');
+      if (name === 'userEmail') {
+          userEmail = value;
+          break;
+      }
+  }
 
-//   if (userEmail) {
-//       // User is signed in, replace "Sign Up" with user's email
-//       updateHeader(userEmail);
-//   } else {
-//       // User is not signed in, keep "Sign Up" link
-//       updateHeaderSignUp();
-//   }
+  if (userEmail) {
+      // User is signed in, replace "Sign Up" with user's email
+      updateHeader(userEmail);
+  } else {
+      // User is not signed in, keep "Sign Up" link
+      updateHeaderSignUp();
+  }
 
-//   displayCookies(cookies);
-// });
+  displayCookies(cookies);
+});
 
-// function displayCookies(cookies) {
-//   const cookieDisplay = document.getElementById('cookieDisplay');
-//   if (cookieDisplay) {
-//       const cookiesList = cookies.map(cookie => {
-//           const [name, value] = cookie.split('=');
-//           return `${name}: ${value}`;
-//       });
+function displayCookies(cookies) {
+  const cookieDisplay = document.getElementById('cookieDisplay');
+  if (cookieDisplay) {
+      const cookiesList = cookies.map(cookie => {
+          const [name, value] = cookie.split('=');
+          return `${name}: ${value}`;
+      });
 
-//       cookieDisplay.innerHTML = cookiesList.join('<br>');
-//   }
-// }
+      cookieDisplay.innerHTML = cookiesList.join('<br>');
+  }
+}
 
-// function updateHeader(email) {
-//   const headerRight = document.getElementById('headerRight');
-//   headerRight.innerHTML = `
-//       <a href="/">Home</a>
-//       <a href="/stats/">Statistics</a>
-//       <a href="/About">About</a>
-//       <span>${email}</span>
-//       <a href="javascript:signOut()">Sign Out</a>
-//   `;
-// }
+function updateHeader(email) {
+  const headerRight = document.getElementById('headerRight');
+  headerRight.innerHTML = `
+      <a href="/">Home</a>
+      <a href="/stats/">Statistics</a>
+      <a href="/About">About</a>
+      <span>${email}</span>
+      <a href="javascript:signOut()">Sign Out</a>
+  `;
+}
 
-// function updateHeaderSignUp() {
-//   const headerRight = document.getElementById('headerRight');
-//   headerRight.innerHTML = `
-//       <a href="/">Home</a>
-//       <a href="/stats/">Statistics</a>
-//       <a href="/About">About</a>
-//       <a href="/sign_in">Sign Up</a>
-//   `;
-// }
-
-// <script>
-// const resultContainer = document.getElementById("result");
-
-// function fetchData() {
-//   resultContainer.innerHTML = "";
-
-//   const filterInput = document.getElementById("filterInput");
-//   const filter = filterInput.value;
-
-//   const url = "https://" + encodeURIComponent(filter);
-//   const headers = {
-//     method: 'GET',
-//     mode: 'cors',
-//     cache: 'default',
-//     credentials: 'omit',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//   };
-
-//   fetch(url, headers)
-//     .then(response => {
-//       if (response.status !== 200) {
-//         const errorMsg = 'Database response error: ' + response.status;
-//         console.log(errorMsg);
-//         const tr = document.createElement("tr");
-//         const td = document.createElement("td");
-//         td.innerHTML = errorMsg;
-//         tr.appendChild(td);
-//         resultContainer.appendChild(tr);
-//         return;
-//       }
-
-//       response.json().then(data => {
-//         console.log(data);
-
-//       for (const row of data.results) {
-//           console.log(row);
-
-//           const tr = document.createElement("tr");
-
-//           const artist = document.createElement("td");
-//           const track = document.createElement("td");
-//           const image = document.createElement("td");
-//           const preview = document.createElement("td");
-
-//           artist.innerHTML = row.artistName;
-//           track.innerHTML = row.trackName; 
-//           const img = document.createElement("img");
-//           img.src = row.artworkUrl100;
-//           image.appendChild(img);
-
-//           const audio = document.createElement("audio");
-//           audio.controls = true;
-//           const source = document.createElement("source");
-//           source.src = row.previewUrl;
-//           source.type = "audio/mp4";
-//           audio.appendChild(source);
-//           preview.appendChild(audio);
-
-//           tr.appendChild(artist);
-//           tr.appendChild(track);
-//           tr.appendChild(image);
-//           tr.appendChild(preview);
-
-//           resultContainer.appendChild(tr);
-//         }
-//       })
-//     })
-//     .catch(err => {
-//       console.error(err);
-//       const tr = document.createElement("tr");
-//       const td = document.createElement("td");
-//       td.innerHTML = err;
-//       tr.appendChild(td);
-//       resultContainer.appendChild(tr);
-//     });
-// }
-// </script>
-
-
-// function signOut() {
-//   // Remove the "userEmail" cookie
-//   document.cookie = 'userEmail=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-
-//   // Redirect to the sign-in page or any other desired page
-//   window.location.href = '/sign_in';
-// }
+function updateHeaderSignUp() {
+  const headerRight = document.getElementById('headerRight');
+  headerRight.innerHTML = `
+      <a href="/">Home</a>
+      <a href="/stats/">Statistics</a>
+      <a href="/About">About</a>
+      <a href="/sign_in">Sign Up</a>
+  `;
+}
 
 <script>
 const resultContainer = document.getElementById("result");
@@ -222,3 +133,12 @@ function fetchData() {
     });
 }
 </script>
+
+
+function signOut() {
+  // Remove the "userEmail" cookie
+  document.cookie = 'userEmail=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+
+  // Redirect to the sign-in page or any other desired page
+  window.location.href = '/sign_in';
+}
--- assets/search/search.js
+++ assets/search/search.js
@@ -1,43 +1,121 @@
-import logo from '/favicon.ico';
-import './style/scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap'
-import { useState, useEffect } from 'react';
+// import logo from '/favicon.ico';
+// import './style/scss';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap'
+// import { useState, useEffect } from 'react';
 
-function App() {
-    const [ searchInput, setSearchInput ] = useState("");
+// function App() {
+//     const [ searchInput, setSearchInput ] = useState("");
 
-    return {
-        <div className="App">
-            <Container>
-                <InputGroup className="mb-3" size="lg">
-                    <FormControl
-                        placeholder="Search for Artist"
-                        type="input"
-                        onKeyPress={event => {
-                            if (event.key == "Enter") {
-                                console.log("Pressed Enter");
-                            }
-                        }}
-                        onChange={event => setSearchInput(event.target.value)}
-                    />
-                    <Button onClick={() => {console.log("Clicked Button")}}>
-                        Search
-                    </Button>
-                </InputGroup>
-            </Container>
-            <Container>
-                <Row className="mx-2 row row-cols-4">
-                    <Card>
-                        <Card.Img src="#" />
-                        <Card.Body>
-                            <Card.Title>Album Name Here</Card.Title>
-                        </Card.Body>
-                    </Card>
-                </Row>
-            </Container>
-        </div>
-    };
+//     return {
+//         <div className="App">
+//             <Container>
+//                 <InputGroup className="mb-3" size="lg">
+//                     <FormControl
+//                         placeholder="Search for Artist"
+//                         type="input"
+//                         onKeyPress={event => {
+//                             if (event.key == "Enter") {
+//                                 console.log("Pressed Enter");
+//                             }
+//                         }}
+//                         onChange={event => setSearchInput(event.target.value)}
+//                     />
+//                     <Button onClick={() => {console.log("Clicked Button")}}>
+//                         Search
+//                     </Button>
+//                 </InputGroup>
+//             </Container>
+//             <Container>
+//                 <Row className="mx-2 row row-cols-4">
+//                     <Card>
+//                         <Card.Img src="#" />
+//                         <Card.Body>
+//                             <Card.Title>Album Name Here</Card.Title>
+//                         </Card.Body>
+//                     </Card>
+//                 </Row>
+//             </Container>
+//         </div>
+//     };
+// }
+
+// export default App;
+
+const resultContainer = document.getElementById("result");
+
+function fetchData() {
+  resultContainer.innerHTML = "";
+
+  const filterInput = document.getElementById("filterInput");
+  const filter = filterInput.value;
+
+  const url = "https://api.spotify.com/v1/search" + encodeURIComponent(filter);
+  const headers = {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'default',
+    credentials: 'omit',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  };
+
+  fetch(url, headers)
+    .then(response => {
+      if (response.status !== 200) {
+        const errorMsg = 'Database response error: ' + response.status;
+        console.log(errorMsg);
+        const tr = document.createElement("tr");
+        const td = document.createElement("td");
+        td.innerHTML = errorMsg;
+        tr.appendChild(td);
+        resultContainer.appendChild(tr);
+        return;
+      }
+
+      response.json().then(data => {
+        console.log(data);
+
+      for (const row of data.results) {
+          console.log(row);
+
+          const tr = document.createElement("tr");
+
+          const artist = document.createElement("td");
+          const track = document.createElement("td");
+          const image = document.createElement("td");
+          const preview = document.createElement("td");
+
+          artist.innerHTML = row.artistName;
+          track.innerHTML = row.trackName; 
+          const img = document.createElement("img");
+          img.src = row.artworkUrl100;
+          image.appendChild(img);
+
+          const audio = document.createElement("audio");
+          audio.controls = true;
+          const source = document.createElement("source");
+          source.src = row.previewUrl;
+          source.type = "audio/mp4";
+          audio.appendChild(source);
+          preview.appendChild(audio);
+
+          tr.appendChild(artist);
+          tr.appendChild(track);
+          tr.appendChild(image);
+          tr.appendChild(preview);
+
+          resultContainer.appendChild(tr);
+        }
+      })
+    })
+    .catch(err => {
+      console.error(err);
+      const tr = document.createElement("tr");
+      const td = document.createElement("td");
+      td.innerHTML = err;
+      tr.appendChild(td);
+      resultContainer.appendChild(tr);
+    });
 }
-
-export default App;
