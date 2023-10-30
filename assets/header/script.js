b4e35a54e0df1d01bfbccec3e200efb4c496140c
EdwinKuttappi <edwinab0007@gmail.com>
2023-10-30 08:50:40 -0700

    Added frontend to search

--- assets/header/script.js
+++ assets/header/script.js
@@ -54,6 +54,87 @@ function updateHeaderSignUp() {
   `;
 }
 
+<script>
+const resultContainer = document.getElementById("result");
+
+function fetchData() {
+  resultContainer.innerHTML = "";
+
+  const filterInput = document.getElementById("filterInput");
+  const filter = filterInput.value;
+
+  const url = "https://" + encodeURIComponent(filter);
+  const headers = {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'default',
+    credentials: 'omit',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  };
+
+  fetch(url, headers)
+    .then(response => {
+      if (response.status !== 200) {
+        const errorMsg = 'Database response error: ' + response.status;
+        console.log(errorMsg);
+        const tr = document.createElement("tr");
+        const td = document.createElement("td");
+        td.innerHTML = errorMsg;
+        tr.appendChild(td);
+        resultContainer.appendChild(tr);
+        return;
+      }
+
+      response.json().then(data => {
+        console.log(data);
+
+      for (const row of data.results) {
+          console.log(row);
+
+          const tr = document.createElement("tr");
+
+          const artist = document.createElement("td");
+          const track = document.createElement("td");
+          const image = document.createElement("td");
+          const preview = document.createElement("td");
+
+          artist.innerHTML = row.artistName;
+          track.innerHTML = row.trackName; 
+          const img = document.createElement("img");
+          img.src = row.artworkUrl100;
+          image.appendChild(img);
+
+          const audio = document.createElement("audio");
+          audio.controls = true;
+          const source = document.createElement("source");
+          source.src = row.previewUrl;
+          source.type = "audio/mp4";
+          audio.appendChild(source);
+          preview.appendChild(audio);
+
+          tr.appendChild(artist);
+          tr.appendChild(track);
+          tr.appendChild(image);
+          tr.appendChild(preview);
+
+          resultContainer.appendChild(tr);
+        }
+      })
+    })
+    .catch(err => {
+      console.error(err);
+      const tr = document.createElement("tr");
+      const td = document.createElement("td");
+      td.innerHTML = err;
+      tr.appendChild(td);
+      resultContainer.appendChild(tr);
+    });
+}
+</script>
+
+
 function signOut() {
   // Remove the "userEmail" cookie
   document.cookie = 'userEmail=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
