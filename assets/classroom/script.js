6c5322872c5e9e6b644757760ee48ae24f468488
Toby-Leeder <tobyzeavleeder@gmail.com>
2023-10-30 20:36:55 -0700

    stupid

--- assets/classroom/script.js
+++ assets/classroom/script.js
@@ -74,13 +74,11 @@ function onConnected() {
         JSON.stringify({sender: username, type: 'JOIN'})
     )
 
-    connectingElement.classList.add('hidden');
 }
 
 
 function onError(error) {
-    connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
-    connectingElement.style.color = 'red';
+    console.log(error)
 }
 
 
