3e55badb321d0f03f1c5760d5072ffba53b5c75e
Aiden Huynh <ah5993909@gmail.com>
2023-12-20 11:40:00 -0800

    Catch unauthorized jwt

--- assets/sign-in/script.js
+++ assets/sign-in/script.js
@@ -3,9 +3,15 @@ document.getElementById('signInForm').addEventListener('submit', function(event)
 
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
+    var url = "http://127.0.0.1:8087/human/authenticate"
+    
+        if (!window.location.href.includes("127.0.0.1")) {
+            url = "https://cj-backend.stu.nighthawkcodingsociety.com/human/authenticate"
+        }
 
     // Make a POST request to your authentication endpoint
-    fetch('https://cj-backend.stu.nighthawkcodingsociety.com/human/authenticate', {
+
+    fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -14,17 +20,23 @@ document.getElementById('signInForm').addEventListener('submit', function(event)
             email: email,
             password: password
         })
-    })
-    .then(response => response.json())
-    .then(data => {
+})
+.then(response => {
+    if (response.status === 401) {
+        alert("Invalid username/password, please try again or make a new account")
+        throw new Error("401: Unauthorized")
+    }
+
+    return response.json()})
+.then(data => {
         // Assuming the response contains a 'token' field
         var token = data.token;
         // Store the token in localStorage or a cookie for future use
         // For simplicity, we're using localStorage here
-        console.log(token);
-        localStorage.setItem('token', token);
+            console.log(token);
+            localStorage.setItem('token', token);
 
-        alert('Sign in successful!');
+            alert('Sign in successful!');
     })
     .catch(error => console.error('Error:', error));
 });
